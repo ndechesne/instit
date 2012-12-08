667bf2b5d4d75926508e98a4f589ef6ddadd0b80
Nicolas Dechesne <n-dechesne@ti.com>
2012-12-08 23:30:39 +0100

    added some validation in eleve
    Signed-off-by: Nicolas Dechesne <n-dechesne@ti.com>

--- app/models/eleve.rb
+++ app/models/eleve.rb
@@ -1,3 +1,8 @@
 class Eleve < ActiveRecord::Base
   attr_accessible :addr, :birth, :firstName, :lastName
+
+  validates :firstName, :presence => true
+  validates :lastName, :presence => true
+
+
 end
