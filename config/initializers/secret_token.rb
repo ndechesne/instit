af3c044e9bd53c03c770f4c746374b5e91784447
Nicolas Dechesne <n-dechesne@ti.com>
2012-12-08 23:15:07 +0100

    created project
    Signed-off-by: Nicolas Dechesne <n-dechesne@ti.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instit::Application.config.secret_token = 'c6bcb98e518e8af0fee800ba8459111525ef3f1590b7190127747cea1e573e78da167a79ec951e2bb3b78fb2a4f06e25df35f5d316ca2499e72c3e01309f0285'
